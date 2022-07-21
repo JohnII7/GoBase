a98ff5ae3c8596f4527ffd7e717d0a7f37326527
JohnII7 <157798378@qq.com>
2022-07-22 01:28:31 +0800

    切片的增删改查

---
+++ base/slice_crud.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func main() {
+	// test1()
+	// test2()
+	// test3()
+	test4()
+}
+
+// add
+func test1() {
+	s1 := []int{}
+	s1 = append(s1, 100)
+	s1 = append(s1, 200)
+	s1 = append(s1, 300)
+	fmt.Printf("s1: %v\n", s1)
+}
+
+// del
+func test2() {
+	s1 := []int{1, 2, 3, 4, 5}
+	// 删除 i 2
+	s1 = append(s1[:2], s1[3:]...)
+	fmt.Printf("s1: %v\n", s1)
+}
+
+// update
+func test3() {
+	s1 := []int{1, 2, 3, 4}
+	s1[1] = 100
+	fmt.Printf("s1: %v\n", s1)
+}
+
+// query
+func test4() {
+	s1 := []int{1, 2, 3, 4}
+	key := 2
+	for i, v := range s1 {
+		if v == key {
+			fmt.Printf("i: %v\n", i)
+		}
+
+	}
+}
