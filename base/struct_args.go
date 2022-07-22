e4eada0abe1cbf829d70cf89617105295f32a29c
JohnII7 <157798378@qq.com>
2022-07-22 21:48:01 +0800

    结构体作为参数

---
+++ base/struct_args.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	id   int
+	name string
+}
+
+func main() {
+	p := &Person{
+		1,
+		"John",
+	}
+	fmt.Printf("p: %v\n", p)
+	fmt.Printf("addr_structFunc(p): %v\n", addr_structFunc(*p))
+
+	fmt.Printf("p: %v\n", p)
+	fmt.Printf("structFunc(p): %v\n", structFunc(p))
+	fmt.Printf("p: %v\n", p)
+
+}
+
+func addr_structFunc(person Person) Person {
+	person.id = -1
+	person.name = "值传递"
+	return person
+}
+
+func structFunc(person *Person) *Person {
+	person.id = 11
+	person.name = "地址传递"
+	return person
+}
