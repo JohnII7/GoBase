93a479d8b0cbe18205045dcc5c484f6a4c99300e
JohnII7 <157798378@qq.com>
2022-07-27 21:11:41 +0800

    std-math

---
+++ std/math/demo.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Printf("math.MaxInt16: %v\n", math.MaxInt16)
+	fmt.Printf("math.Pi: %v\n", math.Pi)
+	fmt.Printf("math.MaxFloat64: %v\n", math.MaxFloat64)
+}
